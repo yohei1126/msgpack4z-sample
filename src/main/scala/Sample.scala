efb52d23c63be688d72e4ac4b6bfd49a79da5dd9
Yohei Onishi <vivre214@gmail.com>
2016-07-11 21:38:11 +0900

    add unpacked val

--- src/main/scala/Sample.scala
+++ src/main/scala/Sample.scala
@@ -1,6 +1,8 @@
 import msgpack4z._
 import msgpack4z.CodecInstances.all._
 
+import scalaz.\/-
+
 object Sample extends App {
   case class Foo(a: Boolean, b: String, c: List[Int])
   val factory = new PackerUnpackerFactory {
@@ -12,10 +14,15 @@ object Sample extends App {
   val instance = mapCodec.codec(Foo.apply _, Foo.unapply _)("a", "b", "c")
 
   val sample = Foo(true, "abcde", List(10, 20, 30))
-  val bytes = instance.toBytes(sample, MsgOutBuffer.create())
-  val unpack1 = MsgpackCodec[MsgpackUnion].unpackAndClose(MsgInBuffer(bytes))
-  println(unpack1)
+  println ("original value = " + sample)
 
-  val unpack2 = instance.unpackAndClose(MsgInBuffer(bytes))
-  println(unpack2)
+  // pack
+  val bytes = instance.toBytes(sample, MsgOutBuffer.create())
+
+  // unpack
+  val unpack1 = instance.unpackAndClose(MsgInBuffer(bytes))
+  unpack1 match {
+    case \/-(Foo(a,b,c)) => println ("unpacked value: a = " + a + ", b = " + b + ", c = " + c)
+    case _ => println ("unpack failed")
+  }
 }
